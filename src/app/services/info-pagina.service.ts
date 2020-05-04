c7c3d487fecf834227e03dc9a4c47a6f6162229c
Orlin Alvarado <oalvaradomorales@gmail.com>
2020-05-03 20:22:52 -0600

    Fin de la seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { Equipo } from '../interfaces/equipo.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +9,27 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
 
   info: InfoPagina = {};
+  equipo: Equipo[] = [];
   cargada = false;
+
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => {
         this.cargada = true;
         this.info = resp;
-        console.log(resp);
       });
   }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-html-dbd46.firebaseio.com/equipo.json')
+      .subscribe( (resp: Equipo[]) => {
+        this.equipo = resp;
+      });
+  }
+
 }
